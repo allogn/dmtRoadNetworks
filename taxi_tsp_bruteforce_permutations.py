2012232d461cdd0409c0379fa3d7f84120914e6d
LukeSkypewalker <work37@gmail.com>
2016-05-23 05:46:39 +0300

    taxi-tsp-permutations refactoring

--- taxi_tsp_bruteforce_permutations.py
+++ taxi_tsp_bruteforce_permutations.py
@@ -1,45 +1,50 @@
 from itertools import permutations
 
-trips = ['A', 'B', 'C', 'D']
-points = trips + trips
-perm = permutations(points)
-pset = set(perm)
 
-for permut in pset.copy():
-    dic = dict.fromkeys(trips, 0)
+def filter_paths(pset):
+    for permut in pset.copy():
+        dic = dict.fromkeys(trips, 0)
 
-    for index, point in enumerate(permut[:-1]):
-        in_car = 0
-        empty_car = 0
-        for val in dic.values():
-            if val == 1:
-                in_car = 1
-                break
+        for index, point in enumerate(permut[:-1]):
+            in_car = 0
+            empty_car = 0
+            for val in dic.values():
+                if val == 1:
+                    in_car = 1
+                    break
 
-        if dic[point] == 0:
-            dic[point] += 1
-        else:
-            dic[point] = 0
+            if dic[point] == 0:
+                dic[point] += 1
+            else:
+                dic[point] = 0
 
-        if sum(dic.values()) == 0:
-            empty_car = 1
+            if sum(dic.values()) == 0:
+                empty_car = 1
 
-        if index > 0 and empty_car > 0:
-            print('empty car')
-            print('permutation removed on step ', index)
-            print(dic.values())
-            pset.remove(permut)
-            break
-
-        if point == permut[index+1]:
-            print('check it')
-            if in_car == 0:
+            if index > 0 and empty_car > 0:
+                print('empty car')
                 print('permutation removed on step ', index)
                 print(dic.values())
                 pset.remove(permut)
                 break
 
-    print(permut)
-    print()
+            if point == permut[index+1]:
+                print('check it')
+                if in_car == 0:
+                    print('permutation removed on step ', index)
+                    print(dic.values())
+                    pset.remove(permut)
+                    break
 
-print(len(pset))
+        print(permut)
+        print()
+    return pset
+
+
+trips = ['A', 'B', 'C', 'D']
+points = trips + trips
+perm = permutations(points)
+pset = set(perm)
+paths = filter_paths(pset)
+
+print(len(paths))
