1446ab1c7ccaee39da74dde180ae4fed6d887606
LukeSkypewalker <work37@gmail.com>
2016-05-24 19:52:11 +0300

    brute fix

--- path.py
+++ path.py
@@ -1,32 +1,4 @@
 class Path:
-    def __init__(self, points):
+    def __init__(self, points, dist):
         self.points = points
-        self.dst = total_distance(points)
-
-    def display(self):
-        print("Src: ", self.src, ", Dst: ", self.dst)
-
-
-def distance(point1, point2):
-    """
-    Returns the Euclidean distance of two points in the Cartesian Plane.
-
-    >>> distance([3,4],[0,0])
-    5.0
-    >>> distance([3,6],[10,6])
-    7.0
-    """
-    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2) ** 0.5
-
-
-def total_distance(points):
-    """
-    Returns the length of the path passing through
-    all the points in the given order.
-
-    >>> total_distance([[1,2],[4,6]])
-    5.0
-    >>> total_distance([[3,6],[7,6],[12,6]])
-    9.0
-    """
-    return sum([distance(point, points[index + 1]) for index, point in enumerate(points[:-1])])
+        self.dist = dist
--- taxi-tsp-bruteforce.py
+++ taxi-tsp-bruteforce.py
@@ -5,27 +5,40 @@ from path import Path
 
 def get_best_path(trips):
 
+    paths = get_all_possible_paths(trips)
+
+    min_dist = float('inf')
+    for path in paths:
+        p = make_path(path, trips)
+        dist = total_distance(p)
+
+        if dist < min_dist:
+            min_dist = dist
+            min_path = path
+
+    best_path = Path(min_path, min_dist)
+
+    return best_path
+
+
+def make_path(path, trips):
+    path_points = []
+    passengers_in_car_from = dict.fromkeys(trips, 0)
+    for i, path_point in enumerate(path[:-1]):
+        path_points.append(path_point.src if passengers_in_car_from[path_point] == 0 else path_point.dst)
+        passengers_in_car_from[path_point] ^= 1  # XOR
+    return path_points
+
+
+def get_all_possible_paths(trips):
     points = trips + trips
     perm = permutations(points)
     path_set = set(perm)
-
     for path in path_set.copy():
-
         if is_path_is_not_valid(path):
             path_set.remove(path)
             continue
-
-    path_points = []
-    passengers_in_car_from = dict.fromkeys(trips, 0)
-
-    for i, path_point in enumerate(path[:-1]):
-
-        path_points.append(path_point.src if passengers_in_car_from[path_point]==0 else path_point.dst)
-        passengers_in_car_from[path_point] ^= 1  # XOR
-
-    best_path = Path(path_points)
-
-    return best_path
+    return path_set
 
 
 def is_path_is_not_valid(path):
@@ -40,6 +53,31 @@ def is_path_is_not_valid(path):
     return False
 
 
+def distance(point1, point2):
+    """
+    Returns the Euclidean distance of two points in the Cartesian Plane.
+
+    >>> distance([3,4],[0,0])
+    5.0
+    >>> distance([3,6],[10,6])
+    7.0
+    """
+    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2) ** 0.5
+
+
+def total_distance(points):
+    """
+    Returns the length of the path passing through
+    all the points in the given order.
+
+    >>> total_distance([[1,2],[4,6]])
+    5.0
+    >>> total_distance([[3,6],[7,6],[12,6]])
+    9.0
+    """
+    return sum([distance(point, points[index + 1]) for index, point in enumerate(points[:-1])])
+
+
 A = Trip((1, 5), (1, 10))
 B = Trip((2, 7), (4, 15))
 C = Trip((5, 8), (3, 17))
@@ -48,7 +86,7 @@ D = Trip((3, 5), (5, 16))
 trips = [A, B, C, D]
 
 best_path = get_best_path(trips)
-print(best_path.dst)
+print(best_path.dist)
 
 
 
