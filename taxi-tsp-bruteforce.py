8660c8d7339f820bf62cfa77edb50413056a60ce
LukeSkypewalker <work37@gmail.com>
2016-05-24 18:51:18 +0300

    taxi-tsp-brute in progress

--- taxi-tsp-bruteforce.py
+++ taxi-tsp-bruteforce.py
@@ -1,14 +1,16 @@
 from itertools import permutations
+from trip import Trip
 
 
-def filter_paths(path_set):
+def get_best_path(path_set):
     for path in path_set.copy():
 
         if is_path_is_not_valid(path):
             path_set.remove(path)
             continue
 
-    return path_set
+
+    return best_path
 
 
 def is_path_is_not_valid(path):
@@ -18,18 +20,38 @@ def is_path_is_not_valid(path):
         is_car_is_empty = True if sum(passengers_in_car.values()) == 0 else False
         passengers_in_car[path_point] ^= 1  # XOR
 
-        if i > 0 and is_car_is_empty :
+        if is_car_is_empty and i > 0:
             return True
 
     return False
 
 
-trips = ['A', 'B', 'C', 'D']
+# A = Trip((1, 5), (1, 10))
+# B = Trip((1, 5), (1, 10))
+# C = Trip((1, 5), (1, 10))
+# D = Trip((1, 5), (1, 10))
+
+A = Trip('A', 'a')
+B = Trip('B', 'b')
+C = Trip('C', 'c')
+D = Trip('D', 'd')
+
+
+# trips = ['A', 'B', 'C', 'D']
+trips = [A, B, C, D]
+
 points = trips + trips
 perm = permutations(points)
 path_set = set(perm)
-filter_paths(path_set)
 
+get_best_path(path_set)
+
+
+
+
+# passengers_in_car = dict.fromkeys(trips, 0)
+# print(passengers_in_car)
+# passengers_in_car[path_point] ^= 1  # XOR
 
 print(*path_set, sep='\n')
 print(len(path_set))
